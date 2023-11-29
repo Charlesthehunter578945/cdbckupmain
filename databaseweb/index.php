9ed2054d2bb4fe26d23df822f555b93018082a9e
Charlesthehunter578945 <krlos7568@gmail.com>
2023-11-29 17:05:57 -0400

    phpwebsite bsc

---
+++ databaseweb/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Database test</title>
+</head>
+<body>
+    <h1>Prueba de base de datos</h1>
+    <br>
+    <button></button>
+    
+</body>
+</html>
